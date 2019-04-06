7b47c50ae5c294785b0bce83337b3fe2dda46205
Cherkasik <ilyakopeysk@gmail.com>
2019-04-06 22:41:07 +0300

    created canvas

---
+++ js.js
@@ -0,0 +1,9 @@
+var canvas = document.createElement('canvas');
+canvas.id = "collage";
+canvas.width = 800;
+canvas.height = 800;
+canvas.style.border = "3px solid black";
+canvas.style.marginLeft = "auto";
+canvas.style.marginRight = "auto";
+canvas.style.display = "block";
+document.body.appendChild(canvas);
