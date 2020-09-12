199b4c65a6a08c147d5f0c395e0b5a20f896a181
Ilya Cherkasov <ilyakopeysk@gmail.com>
2020-09-12 13:56:56 +0300

    making look better

--- js.js
+++ js.js
@@ -1,80 +1,92 @@
-var canvas = document.createElement('canvas');
-canvas.id = "collage";
-canvas.width = 400;
-canvas.height = 400;
-canvas.style.border = "1px solid black";
-canvas.style.margin = "auto";
-canvas.style.display = "block";
-canvas.style.boxShadow = "0 0 20px black"
-document.body.appendChild(canvas);
-var elem = document.getElementById("collage");
-var ctx = canvas.getContext("2d");
-var img1 = new Image; 
-var img2 = new Image;
-var img3 = new Image;
-var img4 = new Image;
-img1.crossOrigin = 'anonymous';
-img2.crossOrigin = 'anonymous';
-img3.crossOrigin = 'anonymous';
-img4.crossOrigin = 'anonymous';
-var rand_x = Math.floor(Math.random()*200 + 100);
-var rand_y = Math.floor(Math.random()*200 + 100);
-img1.src = 'https://source.unsplash.com/random/' + rand_x + 'x' + rand_y;
-img2.src = 'https://source.unsplash.com/random/' + (400 - rand_x) + 'x' + (400 - rand_y);
-img3.src = 'https://source.unsplash.com/random/' + rand_x + 'x' + (400 - rand_y);
-img4.src = 'https://source.unsplash.com/random/' + (400 - rand_x) + 'x' + rand_y;
-img1.onload = function(){ctx.drawImage(img1, 0, 0);}
-img2.onload = function(){ctx.drawImage(img2, rand_x, rand_y);}
-img3.onload = function(){ctx.drawImage(img3, 0, rand_y);}
-img4.onload = function(){ctx.drawImage(img4, rand_x, 0);}   
+const createCanvas = () => {
+    let canv = document.createElement('canvas');
+    canv.id = "collage";
+    canv.width = 400;
+    canv.height = 400;
+    canv.style.cssText = `
+        border: 1px solid black;
+        margin: auto;
+        display: block;
+        box-shadow: 0 0 20px black;
+    `;
+    document.body.appendChild(canv);
+    return canv;
+};
 
-var ref = document.createElement('a');
-var button = document.createElement('button');
-button.appendChild(ref);
-document.body.appendChild(button);
-ref.id = "down-btn";
-ref.href = "#";
-ref.textContent = "Download collage";
-ref.download = "collage.png";
-ref.addEventListener('click', function(){
-    var dataURL = canvas.toDataURL();
-    ref.href = dataURL;
-})
-button.style.margin = "auto";
-button.style.display = "block";
-button.style.marginTop = "40px";
+const addImageToCanvas = (width, height, ctx, coords) => {
+    const img = new Image;
+    img.crossOrigin = 'anonymous';
+    img.src = 'https://source.unsplash.com/random/' + width + 'x' + height;
+    img.onload = () => ctx.drawImage(img, coords.x, coords.y);
+};
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", 'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=ru', true);
-xhr.onload = function(){
-    var text = xhr.responseText;
-    var words = text.split(" ");
-    var countWords = words.length;
-    window.onload = function(){
-    ctx.font = "bold 16pt Comic Sans MS";
-    ctx.fillStyle = "white";
-    ctx.shadowColor = "black"
-    ctx.shadowOffsetX = 3;
-    ctx.shadowOffsetY = 3;
-    ctx.shadowBlur = 4;
-    var marginLeft = 20;
-    if (countWords < 15){var marginTop = 190}
-    else if (countWords < 20){var marginTop = 170}
-    else {var marginTop = 150}
-    var lineHeight = 25;
-    var line = "";
-    for (var n = 0; n < countWords; n++) {
-        var testLine = line + words[n] + " ";
-        var testWidth = ctx.measureText(testLine).width;
-        if (testWidth > 380) {
+const createDownloadButton = (canvas) => {
+    let ref = document.createElement('a');
+    ref.href = canvas.toDataURL();
+    ref.textContent = "Download collage";
+    ref.download = "collage.png";
+    ref.style.cssText = `
+        background-color: green;  
+        border-radius: 5px;
+        color: white;
+        padding: 8px;
+        text-decoration: none;
+        display: block;
+        margin: auto;
+        width: 200px;
+        text-align: center;
+        margin-top: 30px;
+    `;
+    document.body.appendChild(ref);
+};
+
+const writeText = (ctx) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", 'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=ru', true);
+    xhr.onload = () => {
+        const words = xhr.responseText.split(" ");
+        const countWords = words.length;
+        window.onload = () => {
+            ctx.font = "bold 16pt Comic Sans MS";
+            ctx.fillStyle = "white";
+            ctx.shadowColor = "black";
+            ctx.shadowOffsetX = 4;
+            ctx.shadowOffsetY = 4;
+            ctx.shadowBlur = 4;
+            let marginLeft = 20;
+            if (countWords < 15){ var marginTop = 190; }
+            else if (countWords < 20){ var marginTop = 170; }
+            else { var marginTop = 150; }
+            let lineHeight = 25;
+            let line = "";
+            for (let n = 0; n < countWords; n++) {
+                let testLine = line + words[n] + " ";
+                let testWidth = ctx.measureText(testLine).width;
+                if (testWidth > 380) {
+                    ctx.fillText(line, marginLeft, marginTop);
+                    line = words[n] + " ";
+                    marginTop += lineHeight;
+                }
+                else { line = testLine; }
+            }
             ctx.fillText(line, marginLeft, marginTop);
-            line = words[n] + " ";
-            marginTop += lineHeight;
-        }
-        else {
-            line = testLine;
-        }
+        };
     }
-    ctx.fillText(line, marginLeft, marginTop);};
+    xhr.send(null);
+};
+
+const createDemotivator = () => {
+    let canvas = createCanvas();
+    let ctx = canvas.getContext("2d");
+    const rand_x = Math.floor(Math.random()*200 + 100);
+    const rand_y = Math.floor(Math.random()*200 + 100);
+    addImageToCanvas(rand_x, rand_y, ctx, { x: 0, y: 0 });
+    addImageToCanvas(400 - rand_x, 400 - rand_y, ctx, { x: rand_x, y: rand_y });
+    addImageToCanvas(rand_x, 400 - rand_y, ctx, { x: 0, y: rand_y });
+    addImageToCanvas(400 - rand_x, rand_y, ctx, { x: rand_x, y: 0 });
+
+    createDownloadButton(canvas);
+    writeText(ctx);
 }
-xhr.send(null);
+
+createDemotivator();
